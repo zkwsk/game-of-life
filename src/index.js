9e1112aed75f6429ed05b18a228b931096ed2fdc
Zaki Wasik <zaki@webconsu.lt>
2018-11-18 21:41:49 +0100

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import p5 from 'p5';
-import sketch from './src/sketch';
+import sketch from './sketch';
 import 'normalize.css/normalize.css';
 import './styles/index.scss';
 
 // Initialize Game of Life
 new p5(sketch);
-
